321bfebff700333dfddf1a1d971cb33b198493b1
Rodrigo Vega Varas <rvega77@gmail.com>
2023-04-05 20:15:20 -0400

    administracion modo config

--- src/main/java/rvega/bingo/util/ConfiguracionApplication.java
+++ src/main/java/rvega/bingo/util/ConfiguracionApplication.java
@@ -2,6 +2,8 @@ package rvega.bingo.util;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import lombok.Data;
 import rvega.bingo.dominio.TipoModo;
@@ -16,6 +18,7 @@ import rvega.bingo.dominio.TipoModo;
 public class ConfiguracionApplication {
 
     private String titulo;
+    private String url;
     private TipoModo modo;
     private boolean bloqueado;
     private int columnas;
@@ -28,6 +31,14 @@ public class ConfiguracionApplication {
         bloqueado = false;
         columnas = 6;
         filas = 8;
+    }
+
+    public void crearUrl() {
+        // crear url
+        if (url == null) {
+            ExternalContext ext = FacesContext.getCurrentInstance().getExternalContext();
+            url = ext.getRequestScheme() + "://" + ext.getRequestServerName() + ":" + ext.getRequestServerPort() + ext.getRequestContextPath();
+        }
     }
 
     public boolean isModoConfig() {
