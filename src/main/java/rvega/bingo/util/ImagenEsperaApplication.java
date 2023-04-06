cd9421b4846344ebb94b1367288ee9bb5d347fce
Rodrigo Vega Varas <rvega77@gmail.com>
2023-04-06 17:40:21 -0400

    agrega imagenes de espera

---
+++ src/main/java/rvega/bingo/util/ImagenEsperaApplication.java
@@ -0,0 +1,31 @@
+package rvega.bingo.util;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+/**
+ *
+ * @author rodri
+ */
+@Named("imagenUtil")
+@ApplicationScoped
+public class ImagenEsperaApplication {
+
+    private int idx;
+
+    public String getNombre() {
+        if (idx >= DATA.length) {
+            idx = 0;
+        }
+
+        return DATA[idx++];
+    }
+
+    private static final String[] DATA = {
+        "hourglass.gif",
+        "spinner-3.gif",
+        "skateboarding.gif",
+        "rueda-gobierno.gif",
+        "spinner.gif"
+    };
+}
